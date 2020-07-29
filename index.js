396bd822638e19d579bd6b151e650ecb66e7a989
davidvelichko52 <davidvelichko52@gmail.com>
2020-07-28 17:00:49 -0700

    working on nav

---
+++ index.js
@@ -0,0 +1,39 @@
+<nav class="navbar navbar-expand-lg navbar-dark ">
+    <a class="navbar-brand" id="navname">David Velichko</a>
+   c
+
+     <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Dropdown
+      </a>
+      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="dropdown-item" href="#">Action</a>
+        <a class="dropdown-item" href="#">Another action</a>
+        <div class="dropdown-divider"></div>
+        <a class="dropdown-item" href="#">Something else here</a>
+      </div>
+    </li>
+
+    <button class="navbar-toggler bg-transparent text-dark" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse navbar-dark" style="background-color: rgb(88, 134, 177);" id="navbarNav">
+      <ul class="navbar-nav mr-auto">
+        
+        
+
+        <li class="nav-item" id="navpic">
+          <a class="nav-link" href="#aboutme" id="direction">About MySelf</a>
+        </li>
+        <li class="nav-item" id="navpic">
+          <a class="nav-link" href="#skillz" id="direction">Skills</a>
+        </li>
+        <li class="nav-item" id="navpic">
+          <a class="nav-link" href="#projj" id="direction">projects</a>
+        </li>
+ 
+        
+      </ul>
+    </div>
+    
+  </nav> 
